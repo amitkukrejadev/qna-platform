26a329befef6f9e65642371df85e04c179a5c870
Amit Kukreja <thenameisamitkukreja@gmail.com>
2025-06-17 14:01:15 +0530

    Refactor Appwrite SDK initialization to separate module

---
+++ src/models/server/config.ts
@@ -0,0 +1,16 @@
+import env from "../../app/env";
+// Init your Web SDK
+import { Avatars, Client, Databases, Storage, Users } from "node-appwrite";
+
+const client = new Client();
+
+client
+  .setEndpoint(env.appwrite.endpoint) // Your Appwrite Endpoint
+  .setProject(env.appwrite.projectId) // Your project ID
+  .setKey(env.appwrite.apiKey); // Your secret API key
+const database = new Databases(client);
+const avatars = new Avatars(client);
+const storage = new Storage(client);
+const users = new Users(client);
+
+export { client, database, users, avatars, storage };
